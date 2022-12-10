aeb64586284430c655333cdabe1899e4c49f561e
Grigoriy Suslov <bandit474@yandex.ru>
2022-12-10 23:21:26 +0300

    Fixed

--- lesson-1.php
+++ lesson-1.php
@@ -3,22 +3,23 @@ $name = readline("«Как вас зовут?» \n");
 $age = readline("«Сколько вам лет?» \n");
 
 print "«Вас зовут {$name}, вам {$age} лет»\n";
-?>
-<?php
-$name = readline("«Как вас зовут?» \n");
-$questionOne = readline("«Какая задача стоит перед вами сегодня?» \n");
-$timeOne = (int)readline("«Сколько примерно времени эта задача займет?» \n");
-$questionTwo = readline("«Какая задача стоит перед вами сегодня?» \n");
-$timeTwo = (int)readline("«Сколько примерно времени эта задача займет?» \n");
-$questionThree = readline("«Какая задача стоит перед вами сегодня?» \n");
-$timeThree = (int)readline("«Сколько примерно времени эта задача займет?» \n");
 
-//$sum = $timeOne + $timeTwo + $timeThree;
+$name = readline("«Как вас зовут?» \n");
+$task = "«Какая задача стоит перед вами сегодня?» \n";
+$time = "«Сколько примерно времени эта задача займет?» \n";
+
+$questionOne = readline($task);
+$timeOne = (int)readline($time);
+$questionTwo = readline($task);
+$timeTwo = (int)readline($time);
+$questionThree = readline($task);
+$timeThree = (int)readline($time);
+
+$sum = $timeOne + $timeTwo + $timeThree;
 
 echo "{$name}, сегодня у вас запланировано 3 приоритетных задачи на день: \n";
 echo "- {$questionOne} ({$timeOne}ч)\n";
 echo "- {$questionTwo} ({$timeTwo}ч)\n";
 echo "- {$questionThree} ({$timeThree}ч)\n";
-//echo "Примерное время выполнения плана = ({$sum}ч)";
-echo "Примерное время выполнения плана = (" . ($timeOne + $timeTwo + $timeThree) . "ч)";
+echo "Примерное время выполнения плана = ({$sum}ч)";
 ?>
